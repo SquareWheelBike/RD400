c30935e35e9dfbbd8c108a6440aa55e20b0ae0a8
Cole Fuerth <colefuerth@gmail.com>
2022-07-02 01:34:00 -0400

    compiling NFC example

--- example/nfctest/nfctest.ino
+++ example/nfctest/nfctest.ino
@@ -1,35 +1,94 @@
+/*
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS 1    SDA(SS)      ** custom, take a unused pin, only HIGH/LOW required **
+ * SPI SS 2    SDA(SS)      ** custom, take a unused pin, only HIGH/LOW required **
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ */
 
+#include <SPI.h>
 #include <MFRC522.h>
+// #include <SoftwareSerial.h>
 
 // whenever a card with a known UID is detected, toggle the LED
 
-#define LED_PIN 13
+#define LED_PIN 7
 #define RST_PIN 9
-#define SS_PIN 10
+#define SS_PIN 10 // take any pin, use SoftSerial to make it work
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522 rfid(SS_PIN, RST_PIN); // Create MFRC522 instance.
+MFRC522::MIFARE_Key key;
+byte uid[4];
 
 void setup()
 {
-    // initialize the MFRC522
-    mfrc522.PCD_Init();
+    Serial.begin(9600); // Initialize serial communications with the PC (optional)
+    uint16_t stime = millis();
+    while (!Serial && (millis() - stime) < 1000) {
+        delay(1); // wait for serial port to connect. Needed for Leonardo only
+    }
+    Serial.println("Starting SPI");
+    SPI.begin();
+    // generate 3.3V pwm on _3V3_PIN
+    // pinMode(_3V3_PIN, OUTPUT);
+    // analogWrite(_3V3_PIN, 255 * (3.4 / 5.0));
 
-    // set the pinmode of the LED pin to output
     pinMode(LED_PIN, OUTPUT);
-    Serial.println("setup complete");
+    digitalWrite(LED_PIN, LOW);
+    // initialize the MFRC522
+    Serial.println("Initializing MFRC522");
+    rfid.PCD_Init();
+    Serial.println("Setting keybytes");
+    for (byte i = 0; i < 6; i++)
+    {
+        key.keyByte[i] = 0xFF;
+    }
+
+    Serial.println(F("This code scan the MIFARE Classsic NUID."));
+    Serial.print(F("Using the following key:"));
+    printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+
+    // // set the pinmode of the LED pin to output
+    Serial.println("\nsetup complete");
+
+    // flash the LED 5 times fast to indicate that the setup is complete
+    for (int i = 0; i < 5; i++)
+    {
+        digitalWrite(LED_PIN, HIGH);
+        delay(100);
+        digitalWrite(LED_PIN, LOW);
+        delay(100);
+    }
 }
 
 void loop()
 {
-    // check if a card is present
-    if (mfrc522.PICC_IsNewCardPresent())
+    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
     {
-        // if a card is present, try to read its UID
-        if (mfrc522.PICC_ReadCardSerial())
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        for (byte i = 0; i < 4; i++)
         {
-            // if the UID is known, toggle the LED
-            Serial.println("UID: " + String(mfrc522.uid.uidByte[0], HEX) + " " + String(mfrc522.uid.uidByte[1], HEX) + " " + String(mfrc522.uid.uidByte[2], HEX) + " " + String(mfrc522.uid.uidByte[3], HEX));
+            uid[i] = rfid.uid.uidByte[i];
         }
+        printHex(rfid.uid.uidByte, rfid.uid.size);
+        Serial.println();
+        rfid.PICC_HaltA();
+        rfid.PCD_StopCrypto1();
     }
-    delay(1);
+}
+
+void printHex(byte *buffer, byte bufferSize)
+{
+    for (byte i = 0; i < bufferSize; i++)
+    {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
 }
