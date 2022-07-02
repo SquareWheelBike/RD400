36f9e5f814c757602b3e499c9b65f1e852b64afa
Cole Fuerth <colefuerth@gmail.com>
2022-07-02 12:17:13 -0400

    add self test

--- example/nfctest/nfctest.ino
+++ example/nfctest/nfctest.ino
@@ -31,10 +31,11 @@ void setup()
 {
     Serial.begin(9600); // Initialize serial communications with the PC (optional)
     uint16_t stime = millis();
-    while (!Serial && (millis() - stime) < 1000) {
+    while (!Serial && (millis() - stime) < 1000)
+    {
         delay(1); // wait for serial port to connect. Needed for Leonardo only
     }
-    Serial.println("Starting SPI");
+    Serial.println(F("Starting SPI"));
     SPI.begin();
     // generate 3.3V pwm on _3V3_PIN
     // pinMode(_3V3_PIN, OUTPUT);
@@ -43,20 +44,23 @@ void setup()
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, LOW);
     // initialize the MFRC522
-    Serial.println("Initializing MFRC522");
+    Serial.println(F("Initializing MFRC522"));
     rfid.PCD_Init();
-    Serial.println("Setting keybytes");
+    Serial.println(F("Setting keybytes"));
     for (byte i = 0; i < 6; i++)
     {
         key.keyByte[i] = 0xFF;
     }
 
-    Serial.println(F("This code scan the MIFARE Classsic NUID."));
+    Serial.println(F("This code scan the MIFARE Classic NUID."));
     Serial.print(F("Using the following key:"));
     printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 
+    if (rfid.PCD_PerformSelfTest())
+        Serial.println(F("Passed Self-Test"));
+
     // // set the pinmode of the LED pin to output
-    Serial.println("\nsetup complete");
+    Serial.println(F("\nsetup complete"));
 
     // flash the LED 5 times fast to indicate that the setup is complete
     for (int i = 0; i < 5; i++)
@@ -70,18 +74,18 @@ void setup()
 
 void loop()
 {
-    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
+    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+        return;
+
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    for (byte i = 0; i < 4; i++)
     {
-        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-        for (byte i = 0; i < 4; i++)
-        {
-            uid[i] = rfid.uid.uidByte[i];
-        }
-        printHex(rfid.uid.uidByte, rfid.uid.size);
-        Serial.println();
-        rfid.PICC_HaltA();
-        rfid.PCD_StopCrypto1();
+        uid[i] = rfid.uid.uidByte[i];
     }
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
 }
 
 void printHex(byte *buffer, byte bufferSize)
