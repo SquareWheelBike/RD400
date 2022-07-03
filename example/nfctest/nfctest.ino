717b9a23113b1e7234b9dea92870e0ecf4ad8f8d
Cole Fuerth <colefuerth@gmail.com>
2022-07-03 11:47:22 -0400

    working NFC reader demo

--- example/nfctest/nfctest.ino
+++ example/nfctest/nfctest.ino
@@ -21,78 +21,70 @@
 
 #define LED_PIN 7
 #define RST_PIN 9
-#define SS_PIN 10 // take any pin, use SoftSerial to make it work
+#define SS_PIN 10  // take any pin, use SoftSerial to make it work
 
-MFRC522 rfid(SS_PIN, RST_PIN); // Create MFRC522 instance.
+MFRC522 rfid(SS_PIN, RST_PIN);  // Create MFRC522 instance.
 MFRC522::MIFARE_Key key;
 byte uid[4];
 
-void setup()
-{
-    Serial.begin(9600); // Initialize serial communications with the PC (optional)
-    uint16_t stime = millis();
-    while (!Serial && (millis() - stime) < 1000)
-    {
-        delay(1); // wait for serial port to connect. Needed for Leonardo only
-    }
-    Serial.println(F("Starting SPI"));
-    SPI.begin();
-    // generate 3.3V pwm on _3V3_PIN
-    // pinMode(_3V3_PIN, OUTPUT);
-    // analogWrite(_3V3_PIN, 255 * (3.4 / 5.0));
+void setup() {
+  Serial.begin(9600);  // Initialize serial communications with the PC (optional)
+  uint16_t stime = millis();
+  while (!Serial && (millis() - stime) < 2000) {
+    delay(1);  // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.println(F("Starting SPI"));
+  SPI.begin();
+  // generate 3.3V pwm on _3V3_PIN
+  // pinMode(_3V3_PIN, OUTPUT);
+  // analogWrite(_3V3_PIN, 255 * (3.4 / 5.0));
 
-    pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  // initialize the MFRC522
+  Serial.println(F("Initializing MFRC522"));
+  rfid.PCD_Init();
+  Serial.println(F("Setting keybytes"));
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println(F("This code scan the MIFARE Classic NUID."));
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+
+  if (rfid.PCD_PerformSelfTest())
+    Serial.println(F("Passed Self-Test"));
+
+  // // set the pinmode of the LED pin to output
+  Serial.println(F("\nsetup complete"));
+
+  // flash the LED 5 times fast to indicate that the setup is complete
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
     digitalWrite(LED_PIN, LOW);
-    // initialize the MFRC522
-    Serial.println(F("Initializing MFRC522"));
-    rfid.PCD_Init();
-    Serial.println(F("Setting keybytes"));
-    for (byte i = 0; i < 6; i++)
-    {
-        key.keyByte[i] = 0xFF;
-    }
-
-    Serial.println(F("This code scan the MIFARE Classic NUID."));
-    Serial.print(F("Using the following key:"));
-    printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
-
-    if (rfid.PCD_PerformSelfTest())
-        Serial.println(F("Passed Self-Test"));
-
-    // // set the pinmode of the LED pin to output
-    Serial.println(F("\nsetup complete"));
-
-    // flash the LED 5 times fast to indicate that the setup is complete
-    for (int i = 0; i < 5; i++)
-    {
-        digitalWrite(LED_PIN, HIGH);
-        delay(100);
-        digitalWrite(LED_PIN, LOW);
-        delay(100);
-    }
+    delay(100);
+  }
 }
 
-void loop()
-{
-    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
-        return;
+void loop() {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+    return;
 
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    for (byte i = 0; i < 4; i++)
-    {
-        uid[i] = rfid.uid.uidByte[i];
-    }
-    printHex(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-    rfid.PICC_HaltA();
-    rfid.PCD_StopCrypto1();
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  for (byte i = 0; i < 4; i++) {
+    uid[i] = rfid.uid.uidByte[i];
+  }
+  printHex(rfid.uid.uidByte, rfid.uid.size);
+  Serial.println();
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
 }
 
-void printHex(byte *buffer, byte bufferSize)
-{
-    for (byte i = 0; i < bufferSize; i++)
-    {
-        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-        Serial.print(buffer[i], HEX);
-    }
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
 }
